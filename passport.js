a1536550583c624f9c1a99865f97dfa084528f19
nived <nivednadh@gmail.com>
2024-10-05 13:08:57 +0530

    Fix - google auth

--- passport.js
+++ passport.js
@@ -14,7 +14,7 @@ passport.deserializeUser(function(user,done){
 passport.use(new GoogleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
-    callbackURL:"http://beatscarts.shop/auth/google/callback",
+    callbackURL:"https://beatscarts.shop/auth/google/callback",
     passReqToCallback:true
 },
 
